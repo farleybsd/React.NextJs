79615a24ed2940d7cda453ccc2b4fed00aacf82b
farleybsd <farley.t.i@hotmail.com>
2023-10-24 15:15:14 -0300

    V2

--- next/shop/next.config.js
+++ next/shop/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'files.stripe.com',
+    ],
+  },
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
 }
-
 module.exports = nextConfig
