a0d932205e60504f455b96c88b7adc8baf7bf737
farleybsd <farley.t.i@hotmail.com>
2023-11-01 16:56:54 -0300

    V3

--- next/shop/src/pages/product/[id].tsx
+++ next/shop/src/pages/product/[id].tsx
@@ -1,22 +1,75 @@
+import { stripe } from '@/lib/stripe';
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { json } from 'stream/consumers';
+import Stripe from 'stripe';
 
-export default function Product() {
-    const { query } = useRouter();
+interface ProductProps {
+    product: {
+        id: string
+        name: string
+        imageUrl: string
+        price: string
+        description: string
+        defaultPriceId: string
+    }
+
+}
+
+export default function Product({ product }: ProductProps) {
+    function handleBuyProduct() {
+        console.log(product.defaultPriceId)
+    }
     return (
         <ProductContainer>
             <ImageContainer>
-               
+                <Image src={product.imageUrl} width={520} height={480} alt='teste' />
             </ImageContainer>
             <ProductDetails>
-                    <h1>Camiseta x</h1>
-                    <span>R$ 5,00</span>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, voluptate nemo laudantium dolores quidem itaque, veniam qui beatae assumenda quasi exercitationem ut aliquam reprehenderit eius iure quos voluptatem fugiat placeat?</p>
-                    <button>
-                        Compra Agora
-                    </button>
-                </ProductDetails>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+                <p>{product.description}</p>
+                <button onClick={handleBuyProduct}>
+                    Compra Agora
+                </button>
+            </ProductDetails>
         </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            { params: { id: 'prod_OsWR3L9ADg5TvK' } }
+        ],
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+
+    const productId = String(params?.id);
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price'],
+    })
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                }).format(price.unit_amount as number / 100),
+                description: product.description,
+                defaultPriceId: price.id
+            }
+        },
+        revalidate: 60 * 60 * 1 // 1 hours
+    }
 }
